6cd738d3ed1d4232862603f7df78dd54092df1ff
NuggetReckt <84108267+NuggetReckt@users.noreply.github.com>
2022-09-19 17:47:56 +0200

    for sync
    Todo: Projects and Actus styles
    Todo: Index about

--- music.php
+++ music.php
@@ -1,21 +1,20 @@
 <?php
 require_once "assets/php/pager.php";
+require_once "assets/php/database/request.php";
 $pager = new Pager("Accueil");
 $req = new Request();
 
 $pager->setHeader();
 ?>
-
-<div class="top-content" id="top-content-musics">
-    <div id="top-title">
-        <h1>Les musiques</h1>
-        <span>Découvrez les musiques de </span><span style="color: #1394C3">No&Co</span><span> !</span>
-    </div>
-</div>
-<div class="musics">
-    <?php $req->get_musics()?>
-</div>
-
+            <div class="top-content" id="top-content-musics">
+                <div id="top-title">
+                    <h1>Les musiques</h1>
+                    <span>Écoutez les musiques de </span><span style="color: #1394C3">No&Co</span><span> !</span>
+                </div>
+            </div>
+            <div class="musics">
+                <?php $req->get_musics()?>
+            </div>
 <?php
 $pager->setFooter();
 ?>
